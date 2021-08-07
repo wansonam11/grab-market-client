2bdc75e89df728ecb79d3dcb5dd89f2f8ac8f589
rise <sosen7874@naver.com>
2021-08-07 18:36:28 +0900

    Link 태그 적용

--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,15 @@
 import './index.css'
 import axios from 'axios';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
 function MainPage(){
     const [products, setProducts] = React.useState([]);
     React.useEffect(function(){
-        axios.get("gi")
-        .then(function(result){
+        axios
+          .get("https://341cf269-c712-4751-a587-2c7fd1b972ec.mock.pstmn.io/products")
+          .then(function(result){
             const products = result.data.products;
             setProducts(products);
         })
@@ -20,30 +22,32 @@ function MainPage(){
     <div>
     <div id="header">
       <div id="header-area">
-        <img src="images/icons/logo.png" />
+        <img src="images/icons/logo.png"  alt=""/>
       </div>
     </div>
     <div id="body">
       <div id="banner">
-        <img src="images/banners/banner1.png" />
+        <img src="images/banners/banner1.png" alt=""/>
       </div>
       <h1>판매되는 상품들</h1>
       <div id="product-list">
           {products.map(function (product, index){
               return (
         <div className="product-card">
+            <Link className="product-link" to={`/products/${index}`}>
           <div>
             <img className="product-img" 
-            src={product.imageUrl} />
+            src={product.imageUrl} alt="" />
           </div>
           <div className="product-contents">
             <span className="product-name">{product.name}</span>
             <span className="product-price">{product.price}원</span>
             <div className="product-seller">
-              <img className="product-avatar" src="images/icons/avatar.png" />
+              <img className="product-avatar" src="images/icons/avatar.png" alt=""/>
               <span>{product.seller}</span>
             </div>
           </div>
+          </Link>
         </div>
 
 
---
+++ src/product/index.js
@@ -0,0 +1,10 @@
+import {useParams} from "react-router-dom";
+
+function ProductPage(){
+    const {id} = useParams();
+
+    return <h1> 상품 페이지{id} 상품</h1>
+}
+
+
+export default ProductPage; 
